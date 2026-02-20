9eb2a97a477b79de526d919a0436bea491f744e1
Chetankumar Surti <cdsurti@gmail.com>
2026-02-20 11:34:13 +0000

    added singular test to stg_stripe_payment

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -9,10 +9,10 @@ source as (
 renamed as (
 
     select
-        id,
-        user_id,
+        id as order_id,
+        user_id as customer_id,
         order_date,
-        status,
+        status as order_status,
         _etl_loaded_at
 
     from source
@@ -20,3 +20,4 @@ renamed as (
 )
 
 select * from renamed
+
